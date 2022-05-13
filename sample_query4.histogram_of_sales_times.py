1f4b034d22983dbc4f82081653f0c84f8f155226
nicmrayce <62757141+nicmrayce@users.noreply.github.com>
2022-05-14 05:08:38 +0800

    Update sample_query4.histogram_of_sales_times.py

--- sample_query4.histogram_of_sales_times.py
+++ sample_query4.histogram_of_sales_times.py
@@ -5,6 +5,5 @@ df = pd.read_csv (r'C:\Users\Ron\Desktop\Sales_Time.csv')
 df2 = pd.read_csv (r'C:\Users\Ron\Desktop\Sales_Time2.csv')
 
 plt.hist(df, bins=20, alpha=0.4, normed=True)
-#plot your other histogram here
 plt.hist(df2, bins=20, alpha=0.4, normed=True)
 plt.show()
